5651413ed9d246cbd541e9f0fe67579b8d212f61
genoiii <152836107+genoiii@users.noreply.github.com>
2024-06-07 21:11:34 +0800

    Add: Feature 2,3,4. (MS 2 Draft)

---
+++ MotorPHEmployeeApp/src/Class/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Class;
+
+import Frame.LoginPage;
+
+/**
+ *
+ * @author 63909
+ */
+public class Main {
+    public static void main(String args[]) {
+        LoginPage loginPage = new LoginPage();
+        loginPage.setVisible(true);
+        
+    }
+}
